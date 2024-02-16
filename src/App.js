e08a3d503eb599e4627d9a60bb6cd20d33411122
Metta Surendhar <msurendhar8815@gmail.com>
2024-02-16 18:15:22 +0530

    refactored the code for deployment

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
+import { useState } from 'react';
 import './App.css';
+import img1 from './assets/img1.jpg';
+import img2 from './assets/img2.jpg';
 
 function App() {
+	const [appImg, setAppImg] = useState(img1);
 	return (
 		<div className='App'>
-			<header className='App-header'></header>
+			<header className='App-header'>
+				<div className='container'>
+					<h1>Photo Mania</h1>
+					<img
+						src={appImg}
+						alt='an example img'
+						width='500'
+						height='500'
+					/>
+					<div className='btn'>
+						<button onClick={() => setAppImg(img2)}> view </button>
+						<button
+							className='hide'
+							onClick={() => setAppImg(img1)}
+						>
+							{' '}
+							Hide{' '}
+						</button>
+					</div>
+				</div>
+			</header>
 		</div>
 	);
 }
